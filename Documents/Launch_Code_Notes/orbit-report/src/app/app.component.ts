8720938d4e4e9ae86878f632a446a6aa289d30da
Joseph Hendricks <jah8w4@mail.missouri.edu>
2020-08-02 13:44:46 -0500

    created Satellite class

---
+++ Documents/Launch_Code_Notes/orbit-report/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent extends Satellite{
+  title = 'orbit-report';
+}
+
+sourceList: Satellite[];
